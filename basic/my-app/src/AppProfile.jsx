a46984c15eb6f66e7f76867d4d9afdd3b4fd96ab
sugyeongsu <tnrud321@gmail.com>
2023-09-23 11:19:12 +0900

    Update AppProfile.jsx

--- basic/my-app/src/AppProfile.jsx
+++ basic/my-app/src/AppProfile.jsx
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function AppProfile() {
+<<<<<<< Updated upstream
   return <div><p>멀고도 험한 리액트의길</p></div>;
+=======
+  return <div><h2>안녕수경수?</h2></div>;
+>>>>>>> Stashed changes
 }
 
 export default AppProfile;
