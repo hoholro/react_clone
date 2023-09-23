a4066e106db398973619b8296d4be44cd7411d07
hoholro <tnrud321@gmail.com>
2023-09-23 15:23:10 +0900

    컴포넌트시작

--- basic/my-app/src/AppProfile.jsx
+++ basic/my-app/src/AppProfile.jsx
@@ -1,12 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function AppProfile() {
-<<<<<<< Updated upstream
-  return <div><p>멀고도 험한 리액트의길</p></div>;
-=======
-  return <div><h2>안녕수경수?</h2></div>;
->>>>>>> Stashed changes
+  return <div></div>;
 }
 
 export default AppProfile;
