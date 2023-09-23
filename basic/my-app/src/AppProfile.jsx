c8d235b5662a1b269f0d3b0b6c444d7395912a70
hoholro <tnrud321@gmail.com>
2023-09-23 16:16:29 +0900

    프로파일_첫컴포넌트_만들기

--- basic/my-app/src/AppProfile.jsx
+++ basic/my-app/src/AppProfile.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function AppProfile() {
-  return <div></div>;
+  return (
+    <div className="profile">
+      <img
+        className="photo"
+        src="https://i.pinimg.com/474x/3f/11/75/3f1175afc799a71c207bd8538e53d1c8.jpg"
+        alt="profile"
+      />
+
+      <h1>수경수</h1>
+      <p>프론트엔드 개발자</p>
+    </div>
+  );
 }
 
 export default AppProfile;
