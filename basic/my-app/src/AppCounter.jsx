d31d0b26fdd42db8db4ae46ac505349bcd52588a
hoholro <tnrud321@gmail.com>
2023-09-25 22:02:59 +0900

    Update AppCounter.jsx

--- basic/my-app/src/AppCounter.jsx
+++ basic/my-app/src/AppCounter.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import './App.css'
-import Counter from './components/Counter'
-
+import React from "react";
+import "./App.css";
+import Counter from "./components/Counter";
 
 export default function AppCounter() {
-
-
+  const [count, setCount] = useState(0);
   return (
     <div>
-        <Counter/>
+      <div className="container">
+        <div className="banner">
+          Total Count : {count} {count > 10? ''}
+        </div>
+      </div>
+      <Counter />
+      <Counter />
     </div>
   );
 }
