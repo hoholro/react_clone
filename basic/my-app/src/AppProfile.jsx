e8cdede5c0dc458732e5d1e63f2693d64500c689
hoholro <tnrud321@gmail.com>
2023-09-25 21:56:29 +0900

    업데이트
    집에서는 js만 건들자.. 계속 삭제하고 하는것도 힘들다

--- basic/my-app/src/AppProfile.jsx
+++ basic/my-app/src/AppProfile.jsx
@@ -1,54 +1,45 @@
 import "./App.css";
-<<<<<<< Updated upstream
-import Profile from './components/Profile';
-import Avatar from './components/Avatar';
-=======
 import Profile from "./components/Profile";
->>>>>>> Stashed changes
+import Avatar from "./components/Avatar";
 
 export default function AppProfile() {
-  const hanleClick = (event) =>{
+  const hanleClick = (event) => {
     console.log(event);
-    alert('버튼버튼');
-  }
+    alert("버튼버튼");
+  };
   return (
-<<<<<<< Updated upstream
-  <>
-
-  <button onClick={(hanleClick)}>수경수버튼</button>
-  
-  <Avatar 
-  image='https://i.pinimg.com/564x/1f/d3/03/1fd3036613cfff7d62a8e8eb3c55f10e.jpg'
-  isNew={true}
-  />
-  <Profile 
-  image='https://i.pinimg.com/474x/3f/11/75/3f1175afc799a71c207bd8538e53d1c8.jpg'
-  name ='수경수'
-  title ='대부자 개발자'
-  isNew={true}
-  />
-
-  <Profile 
-  image='https://i.pinimg.com/564x/36/64/53/366453ba86e4ca63658260482886473d.jpg'
-  name ='abc'
-  title ='부자부자부자'
-  />
-  
-  <Profile 
-  image='https://i.pinimg.com/564x/d2/ff/56/d2ff56f121284c9f960d1bc5aa1df52b.jpg'
-  name ='abc'
-  title ='천수를 누릴수 있는 부자'
-  isNew={true}
-  />
-  </>
-=======
     <>
+      <button onClick={hanleClick}>수경수버튼</button>
+
+      <Avatar
+        image="https://i.pinimg.com/564x/1f/d3/03/1fd3036613cfff7d62a8e8eb3c55f10e.jpg"
+        isNew={true}
+      />
+      <Profile
+        image="https://i.pinimg.com/474x/3f/11/75/3f1175afc799a71c207bd8538e53d1c8.jpg"
+        name="수경수"
+        title="대부자 개발자"
+        isNew={true}
+      />
+
+      <Profile
+        image="https://i.pinimg.com/564x/36/64/53/366453ba86e4ca63658260482886473d.jpg"
+        name="abc"
+        title="부자부자부자"
+      />
+
+      <Profile
+        image="https://i.pinimg.com/564x/d2/ff/56/d2ff56f121284c9f960d1bc5aa1df52b.jpg"
+        name="abc"
+        title="천수를 누릴수 있는 부자"
+        isNew={true}
+      />
+
       <Profile
         image="https://i.pinimg.com/564x/66/47/33/664733f20e70d1a141ccf4409ec6970b.jpg"
         name="수경수"
         title="대부자개발자"
       />
     </>
->>>>>>> Stashed changes
   );
 }
--- basic/my-app/src/components/Profile.jsx
+++ basic/my-app/src/components/Profile.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import Avatar from './Avatar';
+import React from "react";
+import Avatar from "./Avatar";
 
-<<<<<<< Updated upstream
-export default function Profile({ image, name, title, isNew}) {
+export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
-    {/* js명령문으로 new인 사람만 아이콘 보이게 출력 */}
-    <Avatar image={image} isNew={isNew}/>
-    <h1>{name}</h1>
-    <p>{title}</p>
-  </div>
-);
-=======
-function Profile(props) {
-  return (
-    <div className="profile">
-      <img className="photo" src={props.image} alt="profile" />
-      <h1>{props.name}</h1>
-      <p>{props.title}</p>
+      {/* js명령문으로 new인 사람만 아이콘 보이게 출력 */}
+      <Avatar image={image} isNew={isNew} />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
->>>>>>> Stashed changes
+
+  function Profile(props) {
+    return (
+      <div className="profile">
+        <img className="photo" src={props.image} alt="profile" />
+        <h1>{props.name}</h1>
+        <p>{props.title}</p>
+      </div>
+    );
+  }
 }
